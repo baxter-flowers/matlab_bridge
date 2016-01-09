bcde2e6d45ed5c4c84ccd1e5a4de8906b3b37ab3
buschbapti <baptiste.busch@inria.fr>
2016-01-09 16:17:25 +0100

    Add python side of the bridge

---
+++ setup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='matlab_bridge',
+      version='1.0',
+      packages=['matlab_bridge'],
+      package_dir={'': 'src'})
---
+++ src/matlab_bridge/python_interface.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+import os.path
+import json
+import time
+
+
+class PythonInterface:
+    def __init__(self, matlab_flag='flagMatlabFinished.txt',
+                 python_flag='flagPythonfinished.txt',
+                 shared_folder='/tmp/matlab_bridge/'):
+        # create share_folder if necessary
+        if not os.path.exists(shared_folder):
+            os.makedirs(shared_folder)
+        else:
+            # delete the content of the folder
+            for the_file in os.listdir(shared_folder):
+                file_path = os.path.join(shared_folder, the_file)
+                try:
+                    if os.path.isfile(file_path):
+                        os.unlink(file_path)
+                except Exception, e:
+                    print e
+        # set the flag path
+        self.shared_folder = shared_folder
+        self.matlab_flag = os.path.join(shared_folder, matlab_flag)
+        self.python_flag = os.path.join(shared_folder, python_flag)
+        # define matlab file to read
+        self.matlab_file = os.path.join(shared_folder, 'matlab_file.json')
+        # define python file to send
+        self.python_file = os.path.join(shared_folder, 'python_file.json')
+        # define the rate for sleeping
+        self.rate = 0.01
+
+    def is_matlab_flag_set(self):
+        return os.path.isfile(self.matlab_flag)
+
+    def unset_matlab_flag(self):
+        os.remove(self.matlab_flag)
+
+    def wait_for_matlab_flag(self):
+        # loop until the flag is set
+        while not self.is_matlab_flag_set():
+            time.sleep(self.rate)
+        # unset the flag
+        self.unset_matlab_flag()
+
+    def set_python_flag(self):
+        open(self.python_flag, 'a').close()
+
+    def remove_matlab_file(self):
+        os.remove(self.matlab_file)
+
+    def send(self, data):
+        # write the json file to the shared folder
+        with open(self.python_file, 'w') as output_file:
+            json.dump(data, output_file)
+        # set the python flag
+        self.set_python_flag()
+
+    def read(self):
+        # wait for the flag to be set
+        self.wait_for_matlab_flag()
+        # open the structure
+        with open(self.matlab_file) as data_file:
+            data = json.load(data_file)
+        # remove the read file
+        self.remove_matlab_file()
+        # return the structure
+        return data
