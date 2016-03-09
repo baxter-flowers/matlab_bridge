f372dbbc71e8f4620784c2d0ca62625f6c88c5e6
Yoan Mollard <yoan.mollard@inria.fr>
2016-03-09 15:15:12 +0100

    Fixed \t

--- src/matlab_bridge/python_interface.py
+++ src/matlab_bridge/python_interface.py
@@ -4,13 +4,13 @@ import zmq
 
 class PythonInterface(object):
     def __init__(self, port=5560):
-	self.context = zmq.Context()
-	self.socket = self.context.socket(zmq.REP)
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.REP)
         self.port = port
         self.socket.bind("tcp://*:%s" % self.port)
 
     def send(self, data):
-	self.socket.send(json.dumps(data))
+        self.socket.send(json.dumps(data))
 
     def read(self):
-	return self.socket.recv_json()
+        return self.socket.recv_json()
